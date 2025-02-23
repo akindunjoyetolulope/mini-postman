09fc5ec06de56fa318fd9aa93c43502d9a7fd67f
akindunjoyetolulope <tweetbro99@gmail.com>
2025-02-23 14:59:19 +0100

    add variable table component

--- src/components/ui/VariableTable.tsx
+++ src/components/ui/VariableTable.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
+import { v4 as uuidv4 } from "uuid";
 import { Checkbox, Input } from "antd";
-import "./variableTable.css";
 import { Trash2 } from "lucide-react";
 
 interface Value {
@@ -9,14 +9,13 @@ interface Value {
   description: string;
 }
 
-// interface Props {
-//   title: string;
-// }
-
 export default function VariableTable() {
+  const uniqueId = uuidv4();
+
   const [inputs, setInputs] = React.useState([
     {
-      id: 1,
+      id: uniqueId,
+      checked: false,
       value: {
         key: "",
         value: "",
@@ -25,26 +24,18 @@ export default function VariableTable() {
     },
   ]);
 
-  const handleInputChange = (id: number, newValue: Value) => {
-    setInputs((prev) =>
-      prev.map((input) =>
-        input.id === id ? { ...input, value: newValue } : input
-      )
-    );
-  };
-
   React.useEffect(() => {
     const lastInput = inputs[inputs.length - 1];
     if (
-      (lastInput.value.key.trim() !== "" ||
-        lastInput.value.value.trim() !== "" ||
-        lastInput.value.description.trim() !== "") &&
-      inputs.length > 1
+      lastInput.value.key.trim() !== "" ||
+      lastInput.value.value.trim() !== "" ||
+      lastInput.value.description.trim() !== ""
     ) {
       setInputs((prev) => [
         ...prev,
         {
-          id: prev.length + 1,
+          id: uniqueId,
+          checked: false,
           value: {
             key: "",
             value: "",
@@ -55,19 +46,27 @@ export default function VariableTable() {
     }
   }, [inputs]);
 
-  //   const addInputField = () => {
-  //     setInputs((prev) => [
-  //       ...prev,
-  //       {
-  //         id: prev.length + 1,
-  //         value: {
-  //           key: "",
-  //           value: "",
-  //           description: "",
-  //         },
-  //       },
-  //     ]);
-  //   };
+  const handleInputChange = (id: string, newValue: Value) => {
+    setInputs((prev) =>
+      prev.map((input) =>
+        input.id === id ? { ...input, value: newValue } : input
+      )
+    );
+  };
+
+  const removeInputField = (id: string) => {
+    setInputs((prev) => prev.filter((input) => input.id !== id));
+  };
+
+  const checkInputFieldRow = (id: string) => {
+    setInputs((prev) =>
+      prev.map((input) =>
+        input.id === id ? { ...input, checked: !input.checked } : input
+      )
+    );
+  };
+
+  const lastInputIndex = inputs.length - 1;
 
   return (
     <div>
@@ -76,35 +75,39 @@ export default function VariableTable() {
         <table className="w-full border-collapse border border-red-500">
           <thead>
             <tr className="px-1 py-1">
-              <th className="text-left font-medium border border-gray-300">
-                <div className="flex justify-center">
-                  <Checkbox />
-                </div>
+              <th className="text-left font-medium border border-gray-300 min-w-[30px]">
+                {inputs.length !== 1 ? (
+                  <div className="flex justify-center">
+                    <Checkbox />
+                  </div>
+                ) : null}
               </th>
-              <th className="text-left font-medium px-4 py-2 border border-gray-300">
+              <th className="text-left font-medium px-4 py-2 border border-gray-300 min-w-[150px]">
                 Key
               </th>
-              <th className="text-left font-medium px-4 py-2 border border-gray-300">
+              <th className="text-left font-medium px-4 py-2 border border-gray-300 min-w-[150px]">
                 Value
               </th>
-              <th className="text-left  font-medium px-4 py-2 border border-gray-300">
+              <th className="text-left  font-medium px-4 py-2 border border-gray-300 min-w-[150px]">
                 Description
               </th>
-              <th className="text-right px-4 py-2 border border-gray-300">
+              <th className="text-right px-4 py-2 border border-gray-300 min-w-[150px]">
                 <button type="button">Bulk Edit</button>
               </th>
             </tr>
           </thead>
           <tbody>
-            {inputs.map((input) => (
+            {inputs.map((input, index) => (
               <tr key={input.id}>
                 <td className="px-1 py-1 border-gray-300 border">
-                  <div className="flex justify-center">
-                    {/* {(input.value.key ||
-                      input.value.value ||
-                      input.value.description) && } */}
-                    <Checkbox />
-                  </div>
+                  {lastInputIndex !== index ? (
+                    <div className="flex justify-center">
+                      <Checkbox
+                        checked={input.checked}
+                        onChange={() => checkInputFieldRow(input.id)}
+                      />
+                    </div>
+                  ) : null}
                 </td>
                 <td className="px-1 py-1 border-gray-300 border">
                   <Input
@@ -147,11 +150,17 @@ export default function VariableTable() {
                   />
                 </td>
                 <td className="px-4 py-1 border-gray-300 border">
-                  <div className="flex justify-end">
-                    <button type="button">
-                      <Trash2 size={20} strokeWidth={1} />
-                    </button>
-                  </div>
+                  {lastInputIndex !== index ? (
+                    <div className="flex justify-end">
+                      <button type="button">
+                        <Trash2
+                          size={20}
+                          strokeWidth={1}
+                          onClick={() => removeInputField(input.id)}
+                        />
+                      </button>
+                    </div>
+                  ) : null}
                 </td>
               </tr>
             ))}
